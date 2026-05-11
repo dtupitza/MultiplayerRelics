2d4fa4c5f3ad55fe0b415dfe26dd58fece83a1a4
nageld <nageld@bxscience.edu>
2026-05-10 21:42:49 -0400

    init

---
+++ MultiplayerRelicsCode/Powers/MultiplayerRelicsPower.cs
@@ -0,0 +1,13 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using MultiplayerRelics.MultiplayerRelicsCode.Extensions;
+using Godot;
+
+namespace MultiplayerRelics.MultiplayerRelicsCode.Powers;
+
+public abstract class MultiplayerRelicsPower : CustomPowerModel
+{
+    //Loads from MultiplayerRelics/images/powers/your_power.png
+    public override string CustomPackedIconPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".PowerImagePath();
+    public override string CustomBigIconPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigPowerImagePath();
+}
---
+++ MultiplayerRelicsCode/Relics/MultiplayerRelicsRelic.cs
@@ -0,0 +1,14 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using MultiplayerRelics.MultiplayerRelicsCode.Extensions;
+using Godot;
+
+namespace MultiplayerRelics.MultiplayerRelicsCode.Relics;
+
+public abstract class MultiplayerRelicsRelic : CustomRelicModel
+{
+    //MultiplayerRelics/images/relics
+    public override string PackedIconPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".RelicImagePath();
+    protected override string PackedIconOutlinePath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}_outline.png".RelicImagePath();
+    protected override string BigIconPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigRelicImagePath();
+}
